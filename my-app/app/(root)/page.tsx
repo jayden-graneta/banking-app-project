50fcec1d7fa7f216ddfa564f61627ac43c8a0370
Jayden <jayden.graneta@gmail.com>
2025-05-21 18:27:36 -0400

    1.2

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,18 +1,30 @@
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalancebox from '@/components/TotalBalancebox'
 import React from 'react'
 
 const Home = () => {
   const loggedIn = { firstName: "Jayden"}
+
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
+          Welcome Jayden
+          {/*  
           <HeaderBox 
           type="greeting"
           title="Welcome"
           user= {loggedIn?.firstName || 'Guest'}
           subtext="Access and mange your acount and transactions efficiently"
           />
+          
+
+          <TotalBalancebox
+            accounts ={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+          */}
         </header>
       </div>
     </section>
--- my-app/components/HeaderBox.tsx
+++ my-app/components/HeaderBox.tsx
@@ -6,9 +6,12 @@ const HeaderBox = ({type = 'title', title, subtext, user }: HeaderBoxProps) => {
         <h1 className='header-box-title'>
             {title}
             {type === 'greeting' && (
-                <span className="text-bankGradient">{user}</span>
+                <span className="text-bankGradient">
+                  &nbsp;{user}
+                  </span>
             )}
             </h1>
+            <p className='header-box-subtext'>{subtext}</p>
     </div>
   )
 }
